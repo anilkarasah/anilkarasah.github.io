82ed553b922cfdb393532d1d0fb85464d36cbf84
Anıl Karaşah <anilkarasah@hotmail.com>
2024-08-05 22:39:19 +0300

    Fix configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   outDir: './docs',
-  site: 'https://www.anilkarasah.me'
+  site: 'https://anilkarasah.com'
 })
